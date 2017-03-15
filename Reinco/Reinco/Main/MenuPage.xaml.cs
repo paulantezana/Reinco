749eddd21e590ab128d847337cf1856e02ae70c3
CODE-05 <CODE-05@CODE05>
2017-03-15 11:07:22 -0500

    Se añadieo Navegacion I Login Secillo

---
+++ Reinco/Reinco/Main/MenuPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Reinco.Models;
+using Reinco.Pages.Doing;
+using Reinco.Pages.Owner;
+using Reinco.Pages.Staff;
+using Reinco.Pages.Template;
+using Reinco.Pages.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Reinco.Main
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : ContentPage
+    {
+        public ListView menuListView { get { return listView; } }
+        public MenuPage()
+        {
+            InitializeComponent();
+
+            var items = new List<MenuPageItem>();
+            items.Add(new MenuPageItem
+            {
+                Title = "Perfil",
+                IconSource = "icon.png",
+                TargetType = typeof(UserPage)
+            });
+            items.Add(new MenuPageItem
+            {
+                Title = "Obras",
+                IconSource = "icon.png",
+                TargetType = typeof(DoingPage)
+            });
+            items.Add(new MenuPageItem
+            {
+                Title = "Personal",
+                IconSource = "icon.png",
+                TargetType = typeof(StaffPage)
+            });
+            items.Add(new MenuPageItem
+            {
+                Title = "Plantillas",
+                IconSource = "icon.png",
+                TargetType = typeof(TemplatePage)
+            });
+            items.Add(new MenuPageItem
+            {
+                Title = "Propietario",
+                IconSource = "icon.png",
+                TargetType = typeof(OwnerPage)
+            });
+            listView.ItemsSource = items;
+
+        }
+    }
+}
---
+++ Reinco/Reinco/Pages/Doing/AddDoingPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Reinco.Pages.Doing
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddDoingPage : ContentPage
+    {
+        public AddDoingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
