13b2beb1b41fb4adce69f51072f38f7abb2ea907
CODE-05 <CODE-05@CODE05>
2017-03-18 12:46:16 -0500

    Agrengando ListView A Cada Página OK

--- Reinco/Reinco/Models/MainViewModel.cs
+++ Reinco/Reinco/Models/MainViewModel.cs
@@ -12,14 +12,33 @@ namespace Reinco.Models
 
         #region Properties
         public ObservableCollection<PlantillaItem> plantillaItem { get; set; }
+        public ObservableCollection<PersonalItem> personalItem { get; set; }
+        public ObservableCollection<PropietarioItem> propietarioItem { get; set; }
+        public ObservableCollection<ObraItem> obraItem { get; set; }
+        public ObservableCollection<TareaUsuarioItems> tareaUsuarioItems { get; set; }
+        public ObservableCollection<PlantillaLista> plantillaLista { get; set; }
+        public ObservableCollection<ActividadItems> actividadItems { get; set; }
         #endregion
 
         #region Constructor
         public MainViewModel()
         {
             plantillaItem = new ObservableCollection<PlantillaItem>();
+            personalItem = new ObservableCollection<PersonalItem>();
+            propietarioItem = new ObservableCollection<PropietarioItem>();
+            obraItem = new ObservableCollection<ObraItem>();
+            tareaUsuarioItems = new ObservableCollection<TareaUsuarioItems>();
+            plantillaLista = new ObservableCollection<PlantillaLista>();
+            actividadItems = new ObservableCollection<ActividadItems>();
             CargarPlantillaItem();
+            CargarPersonalItem();
+            CargarPropietarioItem();
+            CargarObraItem();
+            CargarTareaUsuarioItems();
+            CargarPlantillaLista();
+            CargarActividadItems();
         }
+
         #endregion
 
         #region Metodos
@@ -36,7 +55,82 @@ namespace Reinco.Models
                     observacionLevantada = false
                 });
             }
-        } 
+        }
+        private void CargarPersonalItem()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                personalItem.Add(new PersonalItem
+                {
+                    fotoPerfil = "icon.png",
+                    nombre = "Juan",
+                    cargo1 = "gernte",
+                    cargo1Tareas = "(3)",
+                    cargo2 = "Supervisor",
+                    cargo2Tareas = "(1)"
+                });
+            }
+        }
+        private void CargarPropietarioItem()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                propietarioItem.Add(new PropietarioItem
+                {
+                    fotoPerfil = "icon.png",
+                    nombre = "Perico"
+                });
+            }
+        }
+
+        private void CargarObraItem()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                obraItem.Add(new ObraItem
+                {
+                    titulo = "Nombre De La Obra",
+                    responsable = "nombre del responsable",
+                    platilla = "PLANTILLAS"
+                });
+            }
+        }
+
+        private void CargarTareaUsuarioItems()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                tareaUsuarioItems.Add(new TareaUsuarioItems
+                {
+                    titulo = "Titulo De La Tarea",
+                    descripcion = "Descripcion De La Tarea",
+                    numeroTarea = "67"
+                });
+            }
+        }
+        private void CargarPlantillaLista()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                plantillaLista.Add(new PlantillaLista
+                {
+                    codigo = "F08",
+                    nuemroItems = "Items (10)",
+                    agregarActividad = "las"
+                });
+            }
+        }
+        private void CargarActividadItems()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                actividadItems.Add(new ActividadItems
+                {
+                    numeroActividad = Convert.ToString(i),
+                    titulo = "Titulo De La Actividad"
+                });
+            }
+        }
         #endregion
 
     }
