58bfe0169c7c27ed64b247bef5df9eefe0aebdc0
paul yoel <yoel.antezana@gmail.com>
2017-03-24 18:11:09 -0500

    Cambios guardados en crud

--- Reinco/Reinco/Interfaces/Plantilla/PaginaPlantilla.xaml.cs
+++ Reinco/Reinco/Interfaces/Plantilla/PaginaPlantilla.xaml.cs
@@ -18,39 +18,16 @@ namespace Reinco.Interfaces.Plantilla
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PaginaPlantilla : ContentPage
     {
-
-        // DialogService dialog;
-
         public ObservableCollection<PlantillaLista> plantillaLista { get; set; }
 
         public PaginaPlantilla()
         {
             InitializeComponent();
             agregarPlantilla.Clicked += AgregarPlantilla_Clicked;
-
-            CargarPlantillaLista();
-            plantillaLista = new ObservableCollection<PlantillaLista>();
-            CargarPlantillaLista();
-           // plantillaListView.ItemsSource = plantillaLista;
         }
-
-        private void CargarPlantillaLista()
-        {
-            for (int i = 0; i < 15; i++)
-            {
-                plantillaLista.Add(new PlantillaLista
-                {
-                    codigo = "35",
-                    nuemroItems = "15",
-                    agregarActividad = "Items"
-                });
-            }
-        }
-
         private void AgregarPlantilla_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new AgregarPlantilla());
         }
-        
     }
 }
