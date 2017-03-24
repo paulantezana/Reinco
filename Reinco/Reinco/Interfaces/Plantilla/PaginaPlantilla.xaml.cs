ac7a302e7135b5ff1e3372bb8007566124c53984
denis\denis-pc <nahuamel.denis@gmail.com>
2017-03-24 15:29:32 +0100

    agregar cplantilla

--- Reinco/Reinco/Interfaces/Plantilla/PaginaPlantilla.xaml.cs
+++ Reinco/Reinco/Interfaces/Plantilla/PaginaPlantilla.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Reinco.Recursos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,15 +15,18 @@ namespace Reinco.Interfaces.Plantilla
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PaginaPlantilla : ContentPage
     {
+        DialogService dialog;
         public PaginaPlantilla()
         {
             InitializeComponent();
             agregarPlantilla.Clicked += AgregarPlantilla_Clicked;
+            //CargarPlantillas();
         }
 
         private void AgregarPlantilla_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new AgregarPlantilla());
         }
+        
     }
 }
