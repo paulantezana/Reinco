ad6cbde811ab0fa4863a0a8893fa3394a12f22d2
paul yoel <yoel.antezana@gmail.com>
2017-03-24 15:14:40 -0500

    Nuevos cambios

--- Reinco/Reinco/Interfaces/Plantilla/PaginaPlantilla.xaml.cs
+++ Reinco/Reinco/Interfaces/Plantilla/PaginaPlantilla.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Reinco.Gestores;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +14,27 @@ namespace Reinco.Interfaces.Plantilla
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PaginaPlantilla : ContentPage
     {
+        public ObservableCollection<PlantillaLista> plantillaLista { get; set; }
         public PaginaPlantilla()
         {
             InitializeComponent();
             agregarPlantilla.Clicked += AgregarPlantilla_Clicked;
+            plantillaLista = new ObservableCollection<PlantillaLista>();
+            CargarPlantillaLista();
+            plantillaListView.ItemsSource = plantillaLista;
+        }
+
+        private void CargarPlantillaLista()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                plantillaLista.Add(new PlantillaLista
+                {
+                    codigo = "35",
+                    nuemroItems = "15",
+                    agregarActividad = "Items"
+                });
+            }
         }
 
         private void AgregarPlantilla_Clicked(object sender, EventArgs e)
